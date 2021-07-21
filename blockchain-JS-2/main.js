b3a6f3afbc1e4241eea51261b31a82e6f47e2baa
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-20 22:51:06 -0300

    Add blockchain with method longest chain

--- blockchain-JS-2/main.js
+++ blockchain-JS-2/main.js
@@ -6,3 +6,18 @@ class Block {
     this.hash = sha256(this.nonce + this.parentHash).toString()
   }
 }
+
+class Blockchain {
+  longestChain() {
+    const blocks = values(this.blocks)
+    const maxByHeight = maxBy(prop('height'))
+    const maxHeightBlock = reduce(maxByHeight, blocks[0], blocks)
+    const getparent = (x) => {
+      if (x === undefined) {
+        return false
+      }
+      return [x, this.blocks[x.parentHash]]
+    }
+    return reverse(unfold(getParent, maxheightBlock))
+  }
+}
